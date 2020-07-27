9addef30194cddb7c2ce7a37558484521ffb6733
chintan_1881 <chintan123@gmail.com>
2020-07-27 22:49:17 +1000

    cp-branch-

--- src/components/SearchForm.vue
+++ src/components/SearchForm.vue
@@ -1,156 +1,165 @@
 <template>
   <div class="fit column">
-
     <q-card>
-     <q-form greedy ref="searchForm">
+      <q-linear-progress v-show="showProgressBar" indeterminate size="10px" color="secondary" />
+      <q-spinner
+        v-show="showProgressBar"
+        class="z-top fixed-center"
+        color="secondary"
+        size="3em"
+        :thickness="10"
+      />
+      <q-form greedy ref="searchForm">
+        <q-banner rounded dense class="bg-grey-3">
+          <template v-slot:avatar>
+            <q-icon name="search" color="primary" />
+          </template>
+          Search
+        </q-banner>
 
-      <q-banner rounded dense class="bg-grey-3">
-        <template v-slot:avatar>
-          <q-icon name="search" color="primary" />
-        </template>
-        Search
-      </q-banner>
-
-      <q-card-section>
-
-      <q-item>
-        <q-item-section side>
-          Looking for :
-        </q-item-section>
-        <q-item-section>
-      <q-btn-toggle
-        v-model="searchParams.lookingFor"
-        spread
-        no-caps
-        rounded
-        toggle-color="secondary"
-        color="dark"
-        text-color="primary"
-        :options="[
+        <q-card-section>
+          <q-item>
+            <q-item-section side>Looking for :</q-item-section>
+            <q-item-section>
+              <q-btn-toggle
+                v-model="searchParams.lookingFor"
+                spread
+                no-caps
+                rounded
+                toggle-color="secondary"
+                color="dark"
+                text-color="primary"
+                :options="[
           {label: 'Bride', value: '2'},
           {label: 'Groom', value: '1'}
         ]"
-      />
-      <!-- value is aligned to Gender options id 1 : Male; 2 : Female -->
-        </q-item-section>
-      </q-item>
+              />
+              <!-- value is aligned to Gender options id 1 : Male; 2 : Female -->
+            </q-item-section>
+          </q-item>
 
-      <q-item>
-        <q-item-section side>
-          Age :
-        </q-item-section>
-        <q-item-section>
-            <q-range
-              v-model="searchParams.ageFromTo"
-              :min="18"
-              :max="50"
-              label-always
-            />
-        </q-item-section>
-        <q-item-section side>
-            <q-badge color="secondary">
-                Age: {{ searchParams.ageFromTo.min }} to {{ searchParams.ageFromTo.max }}
-            </q-badge>
-        </q-item-section>
-      </q-item>
-            <div class="row">
-              <div class="col">
-                <q-select outlined v-model="searchParams.heightFrom" dense options-dense clearable label="Height(From)"
+          <q-item>
+            <q-item-section side>Age :</q-item-section>
+            <q-item-section>
+              <q-range v-model="searchParams.ageFromTo" :min="18" :max="50" label-always />
+            </q-item-section>
+            <q-item-section side>
+              <q-badge
+                color="secondary"
+              >Age: {{ searchParams.ageFromTo.min }} to {{ searchParams.ageFromTo.max }}</q-badge>
+            </q-item-section>
+          </q-item>
+          <div class="row">
+            <div class="col">
+              <q-select
+                outlined
+                v-model="searchParams.heightFrom"
+                dense
+                options-dense
+                clearable
+                label="Height(From)"
                 :options="heightOptions"
                 :rules="[val => checkHeightFrom(val) || 'Height(To) should be greater than Height(From)']"
-                >
-                </q-select>
-
-              </div>
-              <div class="col">
-                <q-select outlined v-model="searchParams.heightTo" dense options-dense clearable label="Height(To)"
+              ></q-select>
+            </div>
+            <div class="col">
+              <q-select
+                outlined
+                v-model="searchParams.heightTo"
+                dense
+                options-dense
+                clearable
+                label="Height(To)"
                 :options="heightOptions"
                 :rules="[ val => checkHeightTo(val) || 'Height(To) should be greater than Height(From)']"
-                >
-                </q-select>
-              </div>
+              ></q-select>
             </div>
+          </div>
 
-            <div class="row">
-              <div class="col">
-            <q-select v-model="searchParams.maritalStatusPreference"
-              option-value="id"
-              option-label="name"
-              :options="maritalOptions"
-             outlined
-             dense
-             options-dense
-             label="Marital Status"
-             :rules="[val => !!val || 'Field is required']"
-             multiple use-chips
-             input-debounce="0"
-             hint="Hint: Multiple Options can be selected" />
-           </div>
-           <div class="col">
-               <q-select
-               outlined
-               v-model="searchParams.country"
-               :options="countryOptions"
-               dense
-               options-dense
-               label="Country*"
-               option-value="id"
-               option-label="name"
-               :rules="[val => checkCountry(val) || 'Field is required']"
-               input-debounce="0"
-               multiple use-chips
-               clearable />
-           </div>
-           </div>
-            <div class="row">
-
-              <q-space />
-              <q-btn color="primary" label="Search" @click="submitSearchForm" />
+          <div class="row">
+            <div class="col">
+              <q-select
+                v-model="searchParams.maritalStatusPreference"
+                option-value="id"
+                option-label="name"
+                :options="maritalOptions"
+                outlined
+                dense
+                options-dense
+                label="Marital Status"
+                :rules="[val => !!val || 'Field is required']"
+                multiple
+                use-chips
+                input-debounce="0"
+                hint="Hint: Multiple Options can be selected"
+              />
             </div>
-
-           </q-card-section>
-           </q-form>
+            <div class="col">
+              <q-select
+                outlined
+                v-model="searchParams.country"
+                :options="countryOptions"
+                dense
+                options-dense
+                label="Country*"
+                option-value="id"
+                option-label="name"
+                :rules="[val => checkCountry(val) || 'Field is required']"
+                input-debounce="0"
+                multiple
+                use-chips
+                clearable
+              />
+            </div>
+          </div>
+          <div class="row">
+            <q-space />
+            <q-btn color="primary" label="Search" @click="submitSearchForm" />
+          </div>
+        </q-card-section>
+      </q-form>
     </q-card>
   </div>
 </template>
 
 <script>
-
-import axios from 'axios'
+import axios from "axios";
 // import { countryList } from './countryList.js'
-import { mapActions } from 'vuex'
-import mixinFormValidations from 'src/mixins/Mixin_FormValidations.js'
+import { mapActions } from "vuex";
+import mixinFormValidations from "src/mixins/Mixin_FormValidations.js";
 
 export default {
   mixins: [mixinFormValidations],
-  created () {
-    this.createHeightList()
-    this.createAgeFromToList()
+  created() {
+    this.createHeightList();
+    this.createAgeFromToList();
   },
-  mounted () {
+  mounted() {
     axios
-      .get(process.env.API + '/lists')
+      .get(process.env.API + "/lists")
       .then(response => {
-        this.countryList = response.data.country
-        this.countryOptions = this.countryList
-        this.maritalOptions = response.data.marital_status
+        this.countryList = response.data.country;
+        this.countryOptions = this.countryList;
+        this.maritalOptions = response.data.marital_status;
       })
       .catch(error => {
-        console.log(error)
-      }
-      )
+        console.log(error);
+      });
   },
-  data () {
+  data() {
     return {
+      // To show Progress bar when loading
+      showProgressBar: false,
+
       ageFromToOptions: [],
       heightOptions: [],
       countryOptions: [],
       countryList: [],
       martialOptions1: [
-        'Never Married',
-        'Divorced',
-        'Widowed',
-        'Awaiting Divorce'
+        "Never Married",
+        "Divorced",
+        "Widowed",
+        "Awaiting Divorce"
       ],
       maritalOptions: [],
       searchParams: {
@@ -162,92 +171,106 @@ export default {
           min: 20,
           max: 30
         },
-        lookingFor: '2',
-        heightFrom: '',
-        heightTo: '',
+        lookingFor: "2",
+        heightFrom: "",
+        heightTo: "",
         maritalStatusPreference: [],
-        country: [{ id: 81, name: 'India' }] // defaulted to India
+        country: [{ id: 81, name: "India" }] // defaulted to India
       }
-    }
+    };
   },
   methods: {
-    ...mapActions('search', ['saveSearchResults']),
+    ...mapActions("search", ["saveSearchResults"]),
 
-    submitSearchForm () {
-      this.$refs.searchForm.validate().then((success) => {
+    submitSearchForm() {
+      this.showProgressBar = true;
+      //console.log("showProgressBar", this.showProgressBar);
+      this.$refs.searchForm.validate().then(success => {
         if (success) {
-        // console.log("Success");
-          this.fetchSearchResults()
+          // console.log("Success");
+          this.fetchSearchResults();
         } else {
-          console.log('Error')
+          console.log("Error");
         }
-      })
+      });
     },
 
-    async fetchSearchResults () {
+    async fetchSearchResults() {
       // console.log('searchParams', this.searchParams);
-      await this.fetchSearch_fromDB(this.searchParams)
-      this.$emit('fetchSearchResults')
+      await this.fetchSearch_fromDB(this.searchParams);
+      this.$emit("fetchSearchResults");
+      this.showProgressBar = false;
+      //console.log("showProgressBar", this.showProgressBar);
     },
-    fetchSearch_fromDB (data) {
-      return axios.post(process.env.API + '/search', data)
+    fetchSearch_fromDB(data) {
+      return axios
+        .post(process.env.API + "/search", data)
         .then(({ data }) => {
-          // console.log('Search Success', data)
-          this.saveSearchResults(data)
+          //console.log("Search Success", data);
+          this.saveSearchResults(data);
           // Store in Stores
           this.$q.notify({
-            type: 'positive',
-            message: 'Successfully search'
-          })
+            type: "positive",
+            message: "Successfully search"
+          });
         })
         .catch(error => {
-          let errMsg = ''
-          if ('message' in error.response.data) {
-            errMsg = error.response.data.error + ' - ' + error.response.data.message
+          let errMsg = "";
+          if ("message" in error.response.data) {
+            errMsg =
+              error.response.data.error + " - " + error.response.data.message;
           } else {
-            errMsg = error.response.data.error
+            errMsg = error.response.data.error;
           }
-          showErrorMessage(errMsg)
-        })
+          showErrorMessage(errMsg);
+        });
     },
-    createAgeFromToList () {
-      const startAge = 18
-      const endAge = 60
-      let a
+    createAgeFromToList() {
+      const startAge = 18;
+      const endAge = 60;
+      let a;
       for (a = startAge; a <= endAge; a++) {
-        this.ageFromToOptions.push(a)
+        this.ageFromToOptions.push(a);
       }
     },
-    createHeightList () {
+    createHeightList() {
       // create height list when component is created
-      let h
-      let i
-      const startHeight = 4
-      const endHeight = 7
+      let h;
+      let i;
+      const startHeight = 4;
+      const endHeight = 7;
       for (h = startHeight; h <= endHeight; h++) {
         for (i = 0; i <= 12; i++) {
-          this.heightOptions.push(h + ' ft ' + i + ' inches')
+          this.heightOptions.push(h + " ft " + i + " inches");
         }
       }
     },
-    checkHeightTo (heightTo) {
-      const heightFrom = this.searchParams.heightFrom
+    checkHeightTo(heightTo) {
+      const heightFrom = this.searchParams.heightFrom;
       if (heightFrom && heightTo) {
-        return this.compareHeightFromHeightTo(heightFrom, heightTo)
-      } else { return true }
+        return this.compareHeightFromHeightTo(heightFrom, heightTo);
+      } else {
+        return true;
+      }
     },
-    checkHeightFrom (heightFrom) {
-      const heightTo = this.searchParams.heightTo
+    checkHeightFrom(heightFrom) {
+      const heightTo = this.searchParams.heightTo;
       if (heightFrom && heightTo) {
-        return this.compareHeightFromHeightTo(heightFrom, heightTo)
-      } else { return true }
+        return this.compareHeightFromHeightTo(heightFrom, heightTo);
+      } else {
+        return true;
+      }
     },
-    checkCountry (val) {
-    // console.log('Check Country',val, val.length);
-      if (val.length == 0) { return false } else { return true }
+    checkCountry(val) {
+      // console.log('Check Country',val, val.length);
+      if (val.length == 0) {
+        return false;
+      } else {
+        return true;
+      }
     }
   }
-}
+};
 </script>
 
 <style>
