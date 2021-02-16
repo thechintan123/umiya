6de4cdfe29e5c5af13667acbfda009bf9f04ab9a
chintan_1881 <chintan123@gmail.com>
2021-02-16 18:53:50 +1100

    Android App link

---
+++ src/components/GetAndroidApp.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="fit column">
+    <q-card bordered>
+      <banner iconName="fas fa-mobile-alt" bannerTitle="Get Android App" />
+      <q-card-section>
+       You can also use our Android App. It is Free and Easy to use.
+       <br>
+       Click on button below to download from Google play store.
+        <br />
+          <a href="https://play.google.com/store/apps/details?id=com.umiyamatrimony.app" target="_blank">
+          <q-img
+          src="~assets/images/Google-Play-Badge.png"
+          :ratio="1"
+          style="max-width: 150px; height: 75px;"
+          contain
+                  />
+          </a>        
+      </q-card-section> 
+    </q-card>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+    banner: require('./general/Banner.vue').default
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/pages/GetAndroidAppPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page padding>
+    <getAndroidApp/>
+  </q-page>
+</template>
+
+<script>
+import { productName } from '../../package.json'
+
+export default {
+  components: {
+    getAndroidApp: require('components/getAndroidApp.vue').default
+  },
+  meta: {
+    // sets document title
+    title: productName + ' | Get Android App',
+    meta: {
+      description: { name: 'description', content: 'Get Android App. It is free and easy to use.' }
+
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
