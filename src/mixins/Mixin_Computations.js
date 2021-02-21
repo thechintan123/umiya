c651367d88c51b2c43647332b50d09ec1c6c2fab
chintan_1881 <chintan123@gmail.com>
2021-02-21 21:43:19 +1100

    Added Correction comments

--- src/mixins/Mixin_Computations.js
+++ src/mixins/Mixin_Computations.js
@@ -54,5 +54,10 @@ export default {
           .replace('_', '')
       })
     }
+    ,
+    firstLetterUpper(theString) {
+      var newString = theString.toLowerCase().replace(/(^\s*\w|[\.\!\?]\s*\w)/g,function(c){return c.toUpperCase()});
+      return newString;
+    }
   }
 }
